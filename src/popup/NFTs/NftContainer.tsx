356d6e71d7fac24700ec7ef6da1d8d4942e4fa20
Anukul Pandey <anukul.030601@gmail.com>
2024-04-25 15:59:38 +0530

    feat: close btn in dropdown

--- src/popup/NFTs/NftContainer.tsx
+++ src/popup/NFTs/NftContainer.tsx
@@ -1,3 +1,4 @@
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import Uik from '@reef-chain/ui-kit';
 import React, { useState } from 'react';
 
@@ -12,13 +13,16 @@ function NftContainer({ iconUrl, balance, name, id }: Props) {
     const [dropdownStatus, setDropdownStatus] = useState<boolean>(false);
     return (
         <div>
-            <div className='nft-icon__container' onClick={() => setDropdownStatus(!dropdownStatus)}>
+            <div className={dropdownStatus ? "nft-icon__container-no-hover" : `nft-icon__container`} onClick={() => setDropdownStatus(!dropdownStatus)}>
                 <img src={iconUrl} alt="NFT icon" className='nft-icon' />
                 <div className='nft-icon__balance'>{balance}</div>
             </div>
             {dropdownStatus && (
                 <div className='nft-icon__container_dropdown'>
-                    <div className='text-white'>{name}</div>
+                    <div>
+                        <div className='text-white'>{name}</div>
+                        <Uik.Button className="dark-btn nft-container__dropdown-close" onClick={() => setDropdownStatus(false)} icon={faCircleXmark} />
+                    </div>
                     <div className=' nft-icon__container_info'>Balance : {balance}</div>
                     <div className='nft-icon__container_info'>NFT ID : {id}</div>
                 </div>
