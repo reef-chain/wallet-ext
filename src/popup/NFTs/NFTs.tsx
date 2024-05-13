32433d564c3cf6f8ffa526d7ca00297691a24a10
Anukul Pandey <anukul.030601@gmail.com>
2024-05-13 15:43:19 +0530

    feat: _status in nfts

--- src/popup/NFTs/NFTs.tsx
+++ src/popup/NFTs/NFTs.tsx
@@ -1,22 +1,26 @@
 import { hooks, Components, NFT as NFTData } from '@reef-chain/react-lib';
 import Uik from '@reef-chain/ui-kit';
 import { reefState } from '@reef-chain/util-lib';
-import React, { useContext, useState } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import ReefSigners from '../context/ReefSigners';
 import strings from '../../i18n/locales';
 import { useTheme } from '../context/ThemeContext';
 const { NFTCard, OverlayNFT } = Components;
 
 function NFTs() {
-    const nfts = hooks.useObservableState(reefState.selectedNFTs$);
+    const nftsStatus = hooks.useObservableState(reefState.selectedNFTs_status$);
     const [selectedNFT, setSelectedNFT] = useState<NFTData | undefined>(undefined)
     const { accounts, selectedSigner, provider } = useContext(ReefSigners);
     const { isDarkMode } = useTheme();
+    const nfts = nftsStatus?.data.map((val) => val.data);
+    const isLoading = useMemo(() => {
+        return nftsStatus ? !(nftsStatus.getStatus()[0].code == 6) : true;
+    }, [nftsStatus]);
     return (
         <div>
             <Uik.Text text={strings.nfts} />
-            <div className={nfts == undefined ? 'nft_loader' : `nfts-container__list`}>
-                {nfts == undefined ? <Uik.Loading /> : nfts && nfts.length > 0 ? nfts.map((nft) => <div
+            <div className={isLoading ? 'nft_loader' : `nfts-container__list`}>
+                {isLoading ? <Uik.Loading /> : nfts && nfts.length > 0 ? nfts.map((nft) => <div
                     className='nft__button'
                     role="button"
                     onClick={() => setSelectedNFT(nft)}
