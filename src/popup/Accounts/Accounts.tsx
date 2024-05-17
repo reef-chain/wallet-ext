f83e79b1f6cf49f2ffea98f26f6f67247c7c7f7b
Anukul Pandey <anukul.030601@gmail.com>
2024-05-17 15:40:31 +0530

    fix: margin bottom to sticky header

--- src/popup/Accounts/Accounts.tsx
+++ src/popup/Accounts/Accounts.tsx
@@ -31,7 +31,7 @@ const Accounts = (): JSX.Element => {
       {
         selectedAccount && provider && (
           <>
-            <Uik.Text text={"Selected"} className={`${isDarkMode ? "text--dark-mode" : ""}`} />
+            <Uik.Text text={"Selected"} className={`${isDarkMode ? "text--dark-mode" : ""} mt-16`} />
             <Account
               account={selectedAccount}
               isSelected={true}
