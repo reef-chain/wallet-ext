e84b01b37066fff26f359dcb0539b64714335c81
Anukul Pandey <anukul.030601@gmail.com>
2024-06-04 17:20:46 +0530

    fix: changes

--- src/popup/AccountOptions/CreateAccount.tsx
+++ src/popup/AccountOptions/CreateAccount.tsx
@@ -129,30 +129,28 @@ export const CreateAccount = (): JSX.Element => {
         {step === Step.FIRST && (
           <>
             <div className="flex flex-col items-start py-2">
-              <Uik.Label text={strings.generated_12_words} />
+              <Uik.Text type="light" className={`${isDarkMode ? "text--dark-mode" : ""} mb-4`} text={strings.generated_12_words} />
               <textarea
-                className="w-full p-5 rounded-lg bg-zinc-950 border-gray-600 text-primary"
+                className={`w-full p-5 rounded-lg ${isDarkMode ? 'bg-zinc-950' : 'bg-white'} border-gray-600 text-primary`}
                 readOnly
               >
                 {account.suri}
               </textarea>
               <CopyToClipboard
                 text={account.suri}
-                className="hover:cursor-pointer ml-1 py-2"
+                className="hover:cursor-pointer ml-1 py-2 flex"
               >
-                <div title={account.suri}>
+                <div title={account.suri} >
                   <FontAwesomeIcon
                     className={`${isDarkMode ? "text--dark-mode" : "text-[#8f8f8f]"} mr-2`}
                     icon={faCopy as IconProp}
                     size="sm"
                   />
-                  <label className="hover:cursor-pointer">
-                    {strings.copy_to_clipboard}
-                  </label>
+                  <Uik.Text text={strings.copy_to_clipboard} type="mini" className={`${isDarkMode ? "text--dark-mode" : ""}  hover:cursor-pointer`} />
                 </div>
               </CopyToClipboard>
             </div>
-            <WarnMessage className="py-1"
+            <WarnMessage className="py-1" isDarkMode={isDarkMode}
               text={strings.please_write_down}
             />
             <div className="flex align-middle items-center">
@@ -172,7 +170,7 @@ export const CreateAccount = (): JSX.Element => {
         {step === Step.SECOND && (
           <>
             <div className="flex flex-col items-start">
-              <Uik.Label text={strings.name_for_the_acc} />
+              <Uik.Text type="light" className={`ml-5 ${isDarkMode ? "text--dark-mode" : ""}`} text={strings.name_for_the_acc} />
               <Uik.Input
                 className="text-primary rounded-md p-2 w-full"
                 value={account.name}
@@ -186,7 +184,7 @@ export const CreateAccount = (): JSX.Element => {
               )}
             </div>
             <div className="flex flex-col items-start my-3">
-              <Uik.Label text={strings.pass_for_acc} />
+              <Uik.Text type="light" className={`ml-5 ${isDarkMode ? "text--dark-mode" : ""}`} text={strings.pass_for_acc} />
               <Uik.Input
                 className="text-primary rounded-md p-2 w-full"
                 value={password}
@@ -202,7 +200,7 @@ export const CreateAccount = (): JSX.Element => {
             </div>
             {password.length >= 6 && (
               <div className="flex flex-col items-start">
-                <Uik.Label text={strings.repeat_password} />
+                <Uik.Text type="light" className={`ml-5 ${isDarkMode ? "text--dark-mode" : ""}`} text={strings.repeat_password} />
                 <Uik.Input
                   className="text-primary rounded-md p-2 w-full"
                   value={passwordRepeat}
--- src/popup/components/WarnMessage.tsx
+++ src/popup/components/WarnMessage.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import Uik from "@reef-chain/ui-kit";
 
 interface Props {
   text: string;
   className?: string;
+  isDarkMode?: boolean;
 }
 
-export const WarnMessage = ({ text, className }: Props): JSX.Element => {
+export const WarnMessage = ({ text, className, isDarkMode }: Props): JSX.Element => {
   return (
     <div
       className={`flex my-3 border-l-primary border-l-4 pl-2
@@ -18,7 +20,7 @@ export const WarnMessage = ({ text, className }: Props): JSX.Element => {
         className="text-primary mr-2 pt-1"
         icon={faExclamationTriangle as IconProp}
       />
-      <span className="text-left text-gray-300">{text}</span>
+      <Uik.Text text={text} type="mini" className={`${isDarkMode ? "text--dark-mode" : ""} text-left`} />
     </div>
   );
 };
