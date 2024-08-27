8335ddf3fc35bc1e74f2c20b829905101fc50a79
Anukul Pandey <62092256+anukulpandey@users.noreply.github.com>
2024-08-27 21:24:32 +0530

    hide nft balance (#26)

--- src/popup/NFTs/NFTs.tsx
+++ src/popup/NFTs/NFTs.tsx
@@ -1,11 +1,12 @@
 import { hooks, Components, NFT as NFTData } from '@reef-chain/react-lib';
 import Uik from '@reef-chain/ui-kit';
 import { reefState } from '@reef-chain/util-lib';
-import React, { useContext, useMemo, useState } from 'react';
+import React, { useContext, useEffect, useMemo, useState } from 'react';
 import ReefSigners from '../context/ReefSigners';
 import strings from '../../i18n/locales';
 import { useTheme } from '../context/ThemeContext';
 import SqwidButton from './SqwidButton';
+import { useHideBalance } from '../context/HideBalance';
 const { NFTCard, OverlayNFT } = Components;
 
 function NFTs() {
@@ -13,6 +14,7 @@ function NFTs() {
     const [selectedNFT, setSelectedNFT] = useState<NFTData | undefined>(undefined)
     const { accounts, selectedSigner, provider } = useContext(ReefSigners);
     const { isDarkMode } = useTheme();
+    const { isHidden } = useHideBalance()
     const isError = nftsStatus?.hasStatus(reefState.FeedbackStatusCode.ERROR);
 
     const Skeleton = (): JSX.Element => (
@@ -21,6 +23,17 @@ function NFTs() {
             <div className={`nft-skeleton__name${isDarkMode ? '-dark' : ''}`} />
         </div>
     );
+
+    useEffect(() => {
+        const balanceElements = document.querySelectorAll('.nfts__item-balance');
+        balanceElements.forEach(element => {
+            if (isHidden) {
+                (element as any).style.display = 'none';
+            } else {
+                (element as any).style.display = '';
+            }
+        });
+    }, [isHidden, nftsStatus]);
 
     return (
 
@@ -43,6 +56,7 @@ function NFTs() {
                                     iconUrl={nft.data.iconUrl}
                                     name={nft.data.name}
                                     mimetype={nft.data.mimetype}
+                                    isDarkMode={isDarkMode}
                                 />
 
                             </div> : <Skeleton />
